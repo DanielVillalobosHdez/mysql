bc4a5aeb1001d9511dc3cabef0ad1ff6e54f2ba8
DanielVillalobosHdez <danivillalobos1997@gmail.com>
2018-04-15 19:22:04 +0200

    acabado

--- spp.sql
+++ spp.sql
@@ -109,7 +109,7 @@ SELECT NombreProfesor, MAX(Salario) FROM Salarios; /*No se puede utilizar una fu
 SELECT NombreProfesor, MAX(Salario) FROM Salarios WHERE Departamento="Informatica"; /*No se puede utilizar una función de agregación sin el GROUP BY*/
 
 /*8. Escriba una consulta que devuelva el nombre y los salarios (en este orden de columnas) de los 5 mejores profesores con mayores ingresos.*/
-SELECT NombreProfesor, MAX(Salario) FROM Salarios ORDER BY Salario DESC LIMIT 5;/*No se puede utilizar una función de agregación sin el GROUP BY*/
+SELECT NombreProfesor, Salario FROM Salarios ORDER BY Salario DESC LIMIT 5;/*No se puede utilizar una función de agregación sin el GROUP BY*/
 
 /*9. Escriba una consulta que devuelva el nombre y el salario (en este orden de columnas) del profesor con las ganancias más bajas.*/
 SELECT NombreProfesor, MIN(Salario) FROM Salarios; /*No se puede utilizar una función de agregación sin el GROUP BY*/
@@ -128,23 +128,59 @@ SELECT DISTINCT Salario FROM Salarios ORDER BY Salario DESC LIMIT 1 OFFSET 2;
 SELECT DISTINCT a.PrimerNombre, a.SegundoNombre FROM Control AS a, Control AS b WHERE a.SegundoNombre=b.SegundoNombre AND a.PrimerNombre != b.PrimerNombre;
 
 /*14. Escriba una consulta que devuelva el nombre y apellido de todas las personas que retiraron un libro de Terry Crews.*/
-
+SELECT DISTINCT PrimerNombre, SegundoNombre FROM Control JOIN Libros ON Control.IDLibro = Libros.ID WHERE Autor="Terry Crews";
 
 /*15. Escriba una consulta que devuelva el nombre y apellido de todas las personas que se registraron como 'Matar a un ruiseñor' por Harper Lee.*/
-/*16. Escribe una consulta que devuelva el salario promedio de los profesores en la tabla. Cada profesor en el departamento de Ciencias de la Computación acaba de obtener un aumento de 10.000 dólares.*/
-/*17. Escriba una consulta que dé los nombres y los nuevos salarios (en este orden de columnas) de los profesores de Ciencias de la Computación.*/
+SELECT DISTINCT PrimerNombre, SegundoNombre FROM Control JOIN Libros ON Control.IDLibro = Libros.ID WHERE Autor="Harper Lee" AND NombreLibro = "To Kill a Mockingbird"; 
+
+/*16. Escribe una consulta que devuelva el salario promedio de los profesores en la tabla.*/
+SELECT AVG(Salario) FROM Salarios;
+
+/*17. Cada profesor en el departamento de Ciencias de la Computación acaba de obtener un aumento de 10.000 dólares. Escriba una consulta que dé los nombres y los nuevos salarios (en este orden de columnas) de los profesores de Ciencias de la Computación.*/
+SELECT NombreProfesor, Salario +10000 FROM Salarios WHERE Departamento = 'Informatica';
+
 /*18. Escriba una consulta que emita la instrucción SQL utilizada para crear la tabla anterior.*/
+SHOW CREATE TABLE Control;
+SELECT sql FROM sqlite_master WHERE type = 'table' AND tbl_name = 'Control';
+
 /*19. Escriba una consulta que devuelva el nombre y el salario (en este orden de columnas) de todos los profesores que ganan más de 4 veces más que el profesor peor pagado.*/
+SELECT NombreProfesor, Salario FROM Salarios WHERE Salario > (SELECT MIN(Salario) FROM Salarios)*4;
+
 /*20. Escriba una consulta que devuelva la cantidad de autores únicos que han escrito libros en esta tabla.*/
+SELECT COUNT(DISTINCT(Autor)) FROM Libros;
+
 /*21. Escriba una consulta que devuelva el nombre y el salario (en este orden de columnas) de los profesores que ganen entre 120000 y 250000 por año (inclusive).*/
+SELECT NombreProfesro, Salario FROM Salarios WHERE Salario BETWEEN 120000 AND 250000;
+
 /*22. Escriba una consulta que devuelva a todos los profesores (todas las columnas) que trabajan en el departamento de Antropología o ganan más de 150000 al año.*/
+SELECT * FROM Salarios WHERE Departamento = 'Antropologia' OR Salario > 150000;
+
 /*23. Escriba una consulta que devuelva el salario más alto en el departamento de informática.*/
+SELECT MAX(Salario) FROM Salarios WHERE Departamento = "Informatica";
+
 /*24. Escriba una consulta que devuelva la cantidad de profesores que ganan más del doble que el profesor con el salario más bajo en el departamento de Ciencias Políticas.*/
+SELECT  COUNT(*) FROM Salarios WHERE Salario > (SELECT MIN(Salario) FROM Salarios WHERE Departamento = 'Politocas')*2;
+
 /*25. Escriba una consulta que devuelva los nombres de todos los libros prestados por Justin (nombre) Lee (apellido).*/
+SELECT NombreLibro FROM Libros JOIN Control ON Libros.ID = Control.IDLibros WHERE PrimerNombre = 'Justin' AND SegundoNombre = 'Lee';
+
 /*26. Escriba una consulta que devuelva los nombres completos (nombre completo es el primer nombre seguido de un espacio y luego el apellido) de todos los que han sacado un libro. ¡Sin duplicados!*/
+SELECT DISTINT (CONCAT(PrimerNombre, ' ', SegundoNombre) FROM Control;
+
 /*27. Escriba una consulta que devuelva el departamento que más aprovecha junto con el salario agregado total de ese departamento.*/
+SELECT Departamento, SUM(Salario) FROM Salarios GROUP BY Departamento ORDER BY SUM(Salario) DESC LIMIT 1;
+
 /*28. Escriba una consulta que devuelva el departamento con el salario promedio más alto junto con ese salario promedio.*/
+SELECT Departamento, AVG(Salario) FROM Salarios GROUP BY Departamento ORDER BY AVG(Salario) DESC LIMIT 1;
+
 /*29. Escriba una consulta que devuelva el nombre, el departamento y el salario (en este orden de columnas) de cada profesor que tenga el ingreso más alto en su departamento.*/
+SELECT NombreProfesor, Departamento, MAX(Salario) FROM Salarios GROUP BY Departamento;
+
 /*30. Escriba una consulta que devuelva el nombre y el salario (en este orden de columnas) de cada profesor que sea uno de los 3 principales o los 3 principales (incluya lazos).*/
+SELECT NombreProfesor, Salario FROM Salarios WHERE Salario >= (SELECT DISTINCT Salario FROM Salarios ORDER BY Salario DESC LIMIT 1 OFFSET 2) OR Salario <= (SELECT DISTINCT Salario FROM Salarios ORDER BY Salario DESC LIMIT 1 OFFSET 2);
+
 /*31. Escriba una consulta que devuelva todos los departamentos y la cantidad de profesores en cada uno (en este orden de columnas).*/
+SELECT COUNT(*), Departamento FROM Salarios GROUP BY Departamento;
+
 /*32. Escriba una consulta que devuelva el departamento con la mayor cantidad de profesores y esa cantidad (en este orden de columnas).*/
+SELECT COUNT(*), Departamento FROM Salarios GROUP BY Departamento ORDER BY COUNT(*) DESC LIMIT 1;
