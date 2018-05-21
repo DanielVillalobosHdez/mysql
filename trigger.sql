71b448585637c5e25a0d4e2da90e78465a5a2d7e
danielvillaloboshdez <danivillalobos1997@gmail.com>
2018-05-21 12:02:09 +0200

    Trigger, procedure and function

--- trigger.sql
+++ trigger.sql
@@ -1,16 +1,13 @@
 /************************************************************************************************************
-	HECHO POR: Daniel Villalobos y David Gúzman
+	MAKE BY: Daniel Villalobos y David Gúzman
 	
-	FECHA: 20/MAYO/2018
+	DATE: 20/MAYO/2018
 
-	TITULO: USO Y EJEMPLOS DEL TRIGGER
+	TITLE: USE AND EXAMPLES OF THE TRIGGER, PROCEDURE AND FUNCTIONS
 
 *************************************************************************************************************/
 
-
-
-
-
+/**************************************TRIGGER***************************************/
 
 DROP DATABASE IF EXISTS _trigger;
 CREATE DATABASE _trigger;
@@ -64,4 +61,96 @@ INSERT INTO ejemplo VALUES
 SELECT @be AS BeneficioNeto;
 SELECT * FROM ejemplo;
 
+/****************************************************************************************************************/
+
+CREATE TABLE Bote (
+    ID INT(3),
+    Cantidad INT(4),
+    Saldo INT(4)
+);
+
+/*This shit does not work*/
+
+delimiter //
+
+CREATE TRIGGER BoteTrigger AFTER INSERT ON Bote
+  FOR EACH ROW
+    BEGIN
+        SET @bote = @bote + NEW.Cantidad;
+        IF NEW.ID = 3 THEN
+            SET NEW.Saldo = @bote;
+             @bote = 0;
+        END IF;
+    END; //
+delimiter ;
+
+SET @bote = 0;
+
+INSERT INTO Bote (ID, Cantidad) VALUES 
+   (1,200),
+   (2,50),
+   (3,2);
+
+SELECT @Bote;
+
+
+/*SHOW TRIGGERS*/
+
+
+/**************************************PROCEDURE***************************************/
+
+
+
+delimiter //
+
+CREATE PROCEDURE dorepeat(p1 INT)
+  BEGIN
+    SET @x = 0;
+    REPEAT SET @x = @x + 1; UNTIL @x > p1 END REPEAT;
+    END
+  //
+
+delimiter ;
+
+CALL dorepeat(1024);
+SELECT @x;
+
+/**************************************FUNCTION***************************************/
+
+CREATE FUNCTION hello (s CHAR(20))
+
+RETURNS CHAR(50) DETERMINISTIC
+  RETURN CONCAT('Hello, ',s,'!');
+
+SELECT hello('world');
+
+delimiter //
+
+CREATE FUNCTION Meses (num INT(2))
+  BEGIN
+    CASE num
+        WHEN 1
+            CONCAT('ENERO, ', num);
+        WHEN 2
+            CONCAT('FEBRERO, ', num);
+        WHEN 3
+            CONCAT('MARZO, ', num);
+        WHEN 4
+            CONCAT('ENERO, ', num);
+        WHEN 5
+            CONCAT('FEBRERO, ', num);
+        WHEN 6
+             CONCAT('MARZO, ', num);
+        WHEN 7
+             CONCAT('ENERO, ', num);
+        WHEN 8
+             CONCAT('FEBRERO, ', num);
+        WHEN 9
+             CONCAT('MARZO, ', num);
+        WHEN 10                       
+             CONCAT('ENERO, ', num);
+        WHEN 11
+             CONCAT('FEBRERO, ', num);
+        WHEN 12
+             CONCAT('MARZO, ', num);
 
